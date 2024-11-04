946a3e6c3728340e5e7b1a9470121c75455342df
3ARR4 <barrazaaxel2000@gmail.com>
2024-11-03 21:54:18 -0700

    udpating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="header-content">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Mi sitio Web en React</h1>
+        </div>
+      </header>
+
+      <main className="App-body">
+        <p>Explora este interesante video entre Joe Rogan y Donald Trump: </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=hBMoPUAeLnY&t=6320s"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ver Video en YouTube
         </a>
-      </header>
+      </main>
+
+      <footer className="App-footer">
+        <p className="title-footer">
+          Conceptualización de Servicios en la Nube
+        </p>
+        <p>Axel Mauricio Barraza Cárdenas</p>
+        <p>Código de estudiante: 223984261</p>
+        <p>Contacto: barrazaaxel2000@gmail.com</p>
+      </footer>
     </div>
   );
 }
